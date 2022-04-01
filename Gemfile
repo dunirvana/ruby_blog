9dfce8790e6c2641d1268c8f196d011cbde45cdc
Eduardo Teixeira <dunirvana@gmail.com>
2022-04-01 14:10:14 -0300

    Put in18, configure index to show posts.

--- Gemfile
+++ Gemfile
@@ -73,3 +73,7 @@ group :test do
   gem "selenium-webdriver"
   gem "webdrivers"
 end
+
+
+# Custom
+gem "rails-i18n"
--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,9 @@ module Blog
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    # Custom
+    config.i18n.default_locale = 'pt_BR'
+    config.time_zone = 'Brasilia'
   end
 end
